72e90a116336a6d0d2d95be9c3d752b23010c3b8
Meet Zaveri <meethpatel96@gmail.com>
2018-04-17 17:57:44 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://zaverimeet:mlab7771@ds247699.mlab.com:47699/auth-db-node');
+mongoose.connect('mongodb://<username>:<password>@ds247699.mlab.com:47699/auth-db-node');
